9e8d46735c8fcbc89db5398ef13897cb6ef92455
Jejen Juanda <jejen.juanda@gmail.com>
2020-06-15 11:36:10 +0700

    Add files via upload
    pada pembaharuan ini telah di tambahkan sebuah tombol yang dapat di klik, dan jika tombol tersebut di klik, maka akan tampil kalimat "Waalaikum salam" secara singkat.

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,26 @@ package com.example.map;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+private Button tombol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tombol = findViewById(R.id.tombol1);
+// Sebuah Listener untuk penanganan kejadian saat tombol diklik
+        tombol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Memunculkan pesan berupa text dialog saat tombol "Jawab" diklik.
+                Toast.makeText(getApplicationContext(), "Waalaikum salam!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
